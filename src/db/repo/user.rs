c9de62ea027c0ae3c16bfb8fb8889a7deacf14f5
Aitzhanov Ivan <aitvann@gmail.com>
2020-06-08 19:49:33 +0300

    change SECRET_KEY env name to ARGON_SECRET_KEY
    in order not to be confused with JWT_SECRET_KEY

--- src/db/repo/user.rs
+++ src/db/repo/user.rs
@@ -15,7 +15,7 @@ impl<'a> UserPgRepo<'a> {
     pub fn new(connection: &'a PooledConnection<PgConnManager>) -> Self {
         Self { connection }
     }
-    
+
     fn find_query_user_by_username(&self, username: &str) -> Result<Option<QueryUser>, Error> {
         use diesel::result::Error as DieselError;
 
@@ -48,17 +48,15 @@ impl<'a> UserRepo for UserPgRepo<'a> {
     fn find_user_by_username(&self, username: &str) -> Result<Option<User>, Error> {
         let user = self
             .find_query_user_by_username(username)?
-            .map(|query_user| {
-                User {
-                    username: query_user.username,
-                }
+            .map(|query_user| User {
+                username: query_user.username,
             });
 
         Ok(user)
     }
 
     fn find_user_by_credentials(&self, credentials: &LoginUser) -> Result<Option<User>, Error> {
-        let secret_key = env::var("SECRET_KEY").expect("failed to read environment variable");
+        let secret_key = env::var("ARGON_SECRET_KEY").expect("failed to read environment variable");
 
         let quser = if let Some(quser) = self.find_query_user_by_username(credentials.username)? {
             quser
@@ -82,7 +80,11 @@ impl<'a> UserRepo for UserPgRepo<'a> {
         }
     }
 
-    fn update_user(&self, username: &str, user: &crate::domain::user::UpdateUser) -> Result<usize, Error> {
+    fn update_user(
+        &self,
+        username: &str,
+        user: &crate::domain::user::UpdateUser,
+    ) -> Result<usize, Error> {
         let password_hash = hash_password(user.password)?;
 
         let update_user = UpdateUser {
@@ -97,7 +99,7 @@ impl<'a> UserRepo for UserPgRepo<'a> {
 }
 
 fn hash_password(password: &str) -> Result<String, Error> {
-    let secret_key = env::var("SECRET_KEY").expect("failed to read environment variable");
+    let secret_key = env::var("ARGON_SECRET_KEY").expect("failed to read environment variable");
 
     Hasher::default()
         .with_password(password)
