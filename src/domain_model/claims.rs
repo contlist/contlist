729355cd733ea5b685d381d95b5064e853526cc5
Aitzhanov Ivan <aitvann@gmail.com>
2020-11-01 21:43:16 +0300

    add(claims): new function and getters

--- src/domain_model/claims.rs
+++ src/domain_model/claims.rs
@@ -1,7 +1,16 @@
+use chrono::{Duration, Utc};
+use getset::{Getters, MutGetters};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Getters, MutGetters, Debug)]
 pub struct Claims {
-    pub username: String,
+    username: String,
     exp: i64,
+}
+
+impl Claims {
+    pub fn new(username: String, duration: Duration) -> Self {
+        let exp = (Utc::now() + duration).timestamp();
+        Self { username, exp }
+    }
 }
